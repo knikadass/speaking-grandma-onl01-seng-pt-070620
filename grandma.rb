72538a0e2b6920bfad4a4b8da8e7bfbb675920e0
alluring-blog-5170 <knikadass@gmail.com>
2020-07-10 03:36:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@ def speak_to_grandma(string)
  if string == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 elsif string == string.upcase
-  return "HUH?! SPEAK UP, SONNY!"
+  return "NO, NOT SINCE 1938!!"
 else 
-  return "NO, NOT SINCE 1938!"
+  return "HUH?! SPEAK UP, SONNY!"
  end
 end
